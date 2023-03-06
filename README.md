92afb3bd7e1d780e96c200d36a9f34af93cced8f
camilo <camand5@gmail.com>
2023-03-06 18:02:06 -0500

    agregamos dos ambientes virtuales y documentacion del readme

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ Para correr el juego debes seguir las siguientes instrucciones en la terminal:
 cd game
 python3 main.py
 ```
+```sh
+git clone
+cd app
+env/Scripts/activate
+pip install -r requirements.txt
+python main.py
+
+```
--- app/main.py
+++ app/main.py
@@ -17,7 +17,7 @@ def run():
 
   if len(result) > 0:
     country = result[0]
-    print(country)
+    #print(country)
     labels, values = utils.get_population(country)
     charts.generate_bar_chart(country['Country'], labels, values)
 
